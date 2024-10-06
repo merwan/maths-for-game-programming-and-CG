94a7efc08521eec5d21a568d2b476fd159ea54f0
Merouane Atig <merwan@patem.net>
2024-10-06 19:30:50 +0200

    Draw first mesh

--- Chap4/HelloMesh.py
+++ Chap4/HelloMesh.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import DOUBLEBUF, OPENGL
 from OpenGL.GL import glClear, GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT
+from Mesh3D import Mesh3D
 
 pygame.init()
 
@@ -10,11 +11,13 @@ screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPEN
 pygame.display.set_caption("OpenGL in Python")
 done = False
 white = pygame.Color(255, 255, 255)
+mesh = Mesh3D()
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    mesh.draw()
     pygame.display.flip()
 
 pygame.quit()
