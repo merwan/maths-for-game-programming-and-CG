f494e7bef87d3cc8cb83493dfed02dc3d5b33318
Merouane Atig <merwan@patem.net>
2024-10-06 11:00:03 +0200

    Add first two chapters

---
+++ Chap1/hello_window.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+
+screen_width = 1000
+screen_height = 1000
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    pygame.display.update()
+
+pygame.quit()
