cae6e4744d19b658972efd0a99eecdbf9115768f
Merouane Atig <merwan@patem.net>
2024-10-13 11:19:15 +0200

    Add cube mesh

--- Chap4/Mesh3D.py
+++ Chap4/Mesh3D.py
@@ -20,3 +20,31 @@ class Mesh3D:
             glVertex3fv(self.vertices[self.triangles[t + 1]])
             glVertex3fv(self.vertices[self.triangles[t + 2]])
             glEnd()
+
+
+class Cube(Mesh3D):
+    def __init__(self) -> None:
+        self.vertices = [
+            (-0.5, -0.5, 0.5),
+            (0.5, -0.5, 0.5),
+            (0.5, 0.5, 0.5),
+            (-0.5, 0.5, 0.5),
+            (-0.5, -0.5, -0.5),
+            (0.5, -0.5, -0.5),
+            (0.5, 0.5, -0.5),
+            (-0.5, 0.5, -0.5),
+        ]
+        self.triangles = [
+            0, 1, 3,
+            1, 2, 3,
+            1, 5, 2,
+            5 ,6 ,2,
+            4, 5, 6,
+            4, 6, 7,
+            4, 0, 7,
+            0, 3, 7,
+            3, 2, 7,
+            2, 6, 7,
+            0, 1, 4,
+            1, 4, 5,
+        ]
